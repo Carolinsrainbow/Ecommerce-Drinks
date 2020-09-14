11447b5bfbfaebf62442029c3f2a0b652d7d6d26
CarolinsRainbow <carolina__herrera@live.com>
2020-09-14 17:01:18 -0300

    Incorporación del Navbar

--- ec/src/views/Home.vue
+++ ec/src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Inicio del Navbar -->
+    <Navbar></Navbar>
+    <!-- Fin del Navbar -->
+
+    <h1>Bienvenidos</h1>
+    <h2>Nuestros Productos</h2>
+<!-- Inicio del Card -->
+<div class="container">
+  <div class="row">
+    <div clas="col-4">
+       <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+    </div>
+  </div>
+</div>
+<!-- Fin del Card -->
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue"
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navbar
   }
+
 }
 </script>
